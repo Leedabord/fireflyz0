e843ed1e959e1492a7db0ef5c2286609653864d2
Leedabord <gwforumlakers@gmail.com>
2022-05-01 11:57:30 -0700

    1 file modified, 1 added, 2 deleted

--- index.js
+++ index.js
@@ -8,16 +8,23 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import ReactGA from 'react-ga'
 
-import App from './views/App'
+import App from './src/views/App'
 
 console.log('create-react-app env:', process.env.NODE_ENV)
 console.log('firefly project:', process.env.REACT_APP_ENV)
 
 // connect our app to firebase 
 const dbConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  apiKey: "AIzaSyAvB4R09YDHtfxOW7C3OQNLUGi9ce4qrws",
+  authDomain: "todo-firebase-17b28.firebaseapp.com",
+  databaseURL: "https://todo-firebase-17b28.firebaseio.com",
+  projectId: "todo-firebase-17b28",
+  storageBucket: "todo-firebase-17b28.appspot.com",
+  messagingSenderId: "62533171737",
+//  appId: "1:62533171737:web:44045ffb6a9f46327e8888"
+//  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+/  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 }
 Firebase.initializeApp(dbConfig)
 
